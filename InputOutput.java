923c99ddcff89345814fe5e7c6c1a059d472ca0a
rrobii14 <robi.crack1@gmail.com>
2020-04-16 12:57:04 +0800

    Buat Output

--- InputOutput.java
+++ InputOutput.java
@@ -4,7 +4,25 @@ public class InputOutput{
  public static void main(String [] args){
   Scanner scanner =  new Scanner(System.in);
   String nama;
+  int jumlahSks;
+  double IPK;
+
+  System.out.println("== Selamat Datang di Program Hitung Nilai ==");
+  System.out.println("=============== By Robi ====================");
   System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
+  System.out.println("Hai, "+nama);
+
+  System.out.print("Masukkan jumlah SKS yang sudah ditempuh: ");
+  jumlahSks = scanner.nextInt();
+
+  System.out.print("Masukkan IPK sementara :");
+  IPK = scanner.nextDouble();
+
+  double jumlah = IPK * jumlahSks;
+  System.out.println("Total Nilai = "+jumlah);
+ 
+  System.out.println("BERHASIL !");
+
  }
 }
